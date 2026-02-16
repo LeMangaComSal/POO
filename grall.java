e53565f8283cddc2ef573964f2f284d12f738652
João R <joaoricardodasilvaalmeida@gmail.com>
2026-02-15 23:13:14 -0300

    Eba

---
+++ grall.java
@@ -0,0 +1,19 @@
+import static java.lang.System.out;
+import java.util.Scanner;
+
+
+public class grall {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+    
+    double celsius;
+    
+    out.println("Indique a temperatura:");
+    celsius = teclado.nextDouble();
+
+    double kelvin = celsius + 273.15;
+    double farenheit = celsius * 1.8 + 32;
+
+    out.printf("A conversao de %.2f é igual a %.2f em Kelvin e %.2f em farenheit.", celsius, kelvin, farenheit);
+}
+}
